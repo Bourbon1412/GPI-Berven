01875d6e2410737d725b2d8569befe263ae252c3
Bourbon1412 <cvalenzuelac@utem.cl>
2021-10-17 17:29:51 -0300

    Primera parte de la pagina

--- SRC/index.js
+++ SRC/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+
 //settings
-app.set('port',4000);
-app.set('views',path.join(__dirname, 'views'))
-app.engine('html',require('ejs').renderFile)
-app.set('view engine','ejs')
+app.set('port', 4000);
+app.set('views',path.join(__dirname,'views'));
+app.engine('html',require('ejs').renderFile);
+app.set('view engine','ejs');
+
 
 //middlewares
 
@@ -16,9 +18,4 @@ app.use(require('./routes/index'));
 app.use(express.static(path.join(__dirname,'public')));
 
 //listening server
-app.listen(app.get('port'), () => {
-    console.log('Server on port', app.get('port'));
-});
-
-
-
+app.listen(app.get('port'),()=>{});
--- SRC/routes/index.js
+++ SRC/routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const router = express.Router();
+const router =express.Router();
 
-router.get('/', (req,res)=>{
-    res.render('index.html',{title:'Transportes Berven'});
-});
+router.get('/',(req,res)=>{res.render('index.html',{title:"Transportes Berven"});});
+router.get('/contact',(req,res)=>{res.render('contact.html',{title:"Contáctanos"});});
+router.get('/galeria',(req,res)=>{res.render('galeria.html',{title:"Galería"});});
+router.get('/ns',(req,res)=>{res.render('ns.html',{title:"Nuestros Servicios"});});
 
-module.exports = router;
+module.exports=router;
