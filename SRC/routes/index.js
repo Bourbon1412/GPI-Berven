633e24dd7824ee31d062f19f8a2adc13e110d490
Bourbon1412 <cvalenzuelac@utem.cl>
2021-10-29 13:01:39 -0300

    agregar modulo de consultas

--- SRC/routes/index.js
+++ SRC/routes/index.js
@@ -5,5 +5,5 @@ router.get('/',(req,res)=>{res.render('index.html',{title:"Transportes Berven"})
 router.get('/contact',(req,res)=>{res.render('contact.html',{title:"Contáctanos"});});
 router.get('/galeria',(req,res)=>{res.render('galeria.html',{title:"Galería"});});
 router.get('/ns',(req,res)=>{res.render('ns.html',{title:"Nuestros Servicios"});});
-
+router.get('/cotizaciones',(req,res)=>{res.render('cotizaciones.html',{title:"Cotización de Transporte"});});
 module.exports=router;
